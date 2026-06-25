f5d3fbee7b198f4a15637f15e5e20dfe291fc939
tokoprobyarm <deadpoo258@gmail.com>
2026-06-25 13:06:14 +0900

    Delete main.tsx

--- main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TokkuProRoot from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TokkuProRoot />
-  </React.StrictMode>,
-)
